0959ec2409b5da6b8b8739b88b8a9a6ef1446155
Chai07362 <chaitanyaukarande07@gmail.com>
2025-12-26 13:55:21 +0530

    Enhance UI with sample meal display and styling
    Added a new section for displaying a sample meal analyzed by AI NutriCare.

--- app.py
+++ app.py
@@ -36,6 +36,22 @@ Smart calorie & nutrition insights
 </div>
 """, unsafe_allow_html=True)
 
+st.markdown("""
+<div style="
+background: white;
+padding: 15px;
+border-radius: 16px;
+max-width: 700px;
+margin: 20px auto;
+box-shadow: 0 10px 25px rgba(0,0,0,0.1);
+">
+<img src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c"
+     style="width:100%; border-radius:12px;">
+<p style="text-align:center; margin-top:10px; color:#475569;">
+Sample meal analyzed by AI NutriCare
+</p>
+</div>
+""", unsafe_allow_html=True)
 
 input_prompt = """
 You are a certified nutritionist and dietician with expertise in Indian and global cuisines.
@@ -120,6 +136,7 @@ if uploaded_file:
 
         st.subheader("🔎 Analysis")
         st.write(response)
+
 
 
 
